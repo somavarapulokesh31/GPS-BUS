ad15c780919c8dcb0bd314c65be91cb395c7d93b
somavarapulokesh31 <somavarapulokesh31@gmail.com>
2026-09-24 10:13:28 +0530

    Updated code

--- school-bus-tracker/frontend/vite.config.js
+++ school-bus-tracker/frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/GPS-BUS/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
